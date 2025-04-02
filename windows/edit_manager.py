9de1993d9ab71e0dbcbb2a24f48c3bef76901c5e
Тимур <ladnoladno477@gmail.com>
2025-04-02 17:47:42 +0500

    more features

--- windows/edit_manager.py
+++ windows/edit_manager.py
@@ -5,12 +5,15 @@ from sqlalchemy import select, update
 
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QDialog, QMessageBox, QFileDialog
-from PySide6.QtCore import Signal
+from PySide6.QtCore import Signal, Qt, QByteArray
 
 
 class EditProfile(QDialog):
     profileUpdated = Signal()
-    def __init__(self, login: str):
+
+    photo_data = None
+
+    def __init__(self, login: str, photo: bytes | None = None):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
@@ -29,6 +32,13 @@ class EditProfile(QDialog):
         self.ui.lastname.setText(self.user_info.last_name)
         self.ui.number.setText(self.user_info.number)
         self.ui.city.setText(self.user_info.city)
+
+        if photo:
+            pixmap = QPixmap()
+            pixmap.loadFromData(QByteArray(photo))
+            label_size = self.ui.user_image.size()  # Получаем размеры QLabel
+            pixmap = pixmap.scaled(label_size, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+            self.ui.user_image.setPixmap(pixmap)
         
     def take_photo(self):
         """Открывает диалог выбора файла и загружает фото"""
@@ -41,6 +51,8 @@ class EditProfile(QDialog):
 
                 # Отобразить фото в QLabel
                 pixmap = QPixmap(file_path)
+                label_size = self.ui.user_image.size()  # Получаем размеры QLabel
+                pixmap = pixmap.scaled(label_size, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
                 self.ui.user_image.setPixmap(pixmap)
 
             except Exception as e:
@@ -62,19 +74,6 @@ class EditProfile(QDialog):
         lastname = self.ui.lastname.text()
         number = self.ui.number.text()
         city = self.ui.city.text()
-        
-        # Открываем диалог выбора файла
-        file_path, _ = QFileDialog.getOpenFileName(self, "Выберите фото", "", "Images (*.png *.jpg *.jpeg *.bmp *.gif)")
-
-        photo_data = None  # По умолчанию фото отсутствует
-
-        if file_path:
-            try:
-                with open(file_path, "rb") as file:
-                    photo_data = file.read()  # Читаем изображение в бинарном формате
-            except Exception as e:
-                QMessageBox.warning(self, "Ошибка", f"Не удалось загрузить фото: {e}")
-                return
 
         session = local_session()
         try:
@@ -86,14 +85,9 @@ class EditProfile(QDialog):
                         first_name=firstname,
                         last_name=lastname,
                         number=number,
-                        city=city)
+                        city=city,
+                        photo=self.photo_data)
             )
-            
-            # Добавляем фото только если оно выбрано
-            if photo_data:
-                update_query = update_query.values(photo=photo_data)
-
-            result = session.execute(update_query)
             
             # Проверка, были ли изменения
             if result.rowcount > 0:
